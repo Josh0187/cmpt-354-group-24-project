105e21fd6acf51ea3e0c8b91ee4b69fa94c39beb
Joshua Lawrence <parsajoshua1@gmail.com>
2022-07-22 17:13:39 -0700

    some styling and nav bar

--- giftshops.php
+++ giftshops.php
@@ -1,26 +1,56 @@
 <?php
 include 'connect.php';
 $conn = OpenCon();
-$sql = "SELECT * FROM zoosections";
+
+// Nav bar
+echo "   
+<link rel='stylesheet' href='../styles.css'>
+<ul class='nav-list'>
+    <li class='nav-item'><a class='nav-link' href='../index.html'>Dashboard</a></li>
+    <li class='nav-item'><a class='nav-link' href='../sections.php'>Sections</a></li>
+    <li class='nav-item'><a class='nav-link' href='#'>Enclosures</a></li>
+    <li class='nav-item'><a class='nav-link' href='#'>Animals</a></li>
+    <li class='nav-item'><a class='nav-link' href='#'>Events</a></li>
+    <li class='nav-item'><a class='nav-link' href='#'>Guests</a></li>
+    <li class='nav-item'><a class='nav-link' href='#'>Employees</a></li>
+</ul>
+";
+
+// if sectionName given, query only giftshops in given sectionName
+if (isset($_GET['sectionName'])) {
+    $sectionName = $_GET['sectionName'];
+    $sql = "SELECT * FROM giftshops WHERE SectionName='$sectionName'";
+}
+// otherwise get all giftshops
+else {
+    $sectionName = "All Sections";
+    $sql = "SELECT * FROM giftshops";
+}
+
 $result = $conn->query($sql);
 
 echo "
-    <a href='index.html'>Home</a>
+    <h1>Giftshops in section: $sectionName</h1>
 ";
 
 if ($result->num_rows > 0) {
     echo "
-    <ul>
+    <table>
+    <tr>
+        <th class='border-class'>Giftshop Number</th>
+        <th class='border-class'>Section Name</th>
+    </tr>
     ";
     // output data of each row
     while($row = $result->fetch_assoc()) {
         echo "
-            <li>
-                <a href='#'>".$row["SectionName"]."</a>
-            </li>
+            <tr>
+                <td class='border-class'>".$row["GiftshopNum"]."</td>
+                <td class='border-class'>".$row["SectionName"]."</td>
+            </tr>
         ";
     }
-    echo "</ul>";
+    echo "</table>";
 
 } else {
     echo "0 results";
--- sections.php
+++ sections.php
@@ -4,8 +4,19 @@ $conn = OpenCon();
 $sql = "SELECT * FROM zoosections";
 $result = $conn->query($sql);
 
+// nav bar
 echo "
-    <a href='index.html'>Home</a>
+<link rel='stylesheet' href='styles.css'>
+<ul class='nav-list'>
+    <li class='nav-item'><a class='nav-link' href='index.html'>Dashboard</a></li>
+    <li class='nav-item'><a class='nav-link clicked' href='sections.php'>Sections</a></li>
+    <li class='nav-item'><a class='nav-link' href='#'>Enclosures</a></li>
+    <li class='nav-item'><a class='nav-link' href='#'>Animals</a></li>
+    <li class='nav-item'><a class='nav-link' href='#'>Events</a></li>
+    <li class='nav-item'><a class='nav-link' href='#'>Guests</a></li>
+    <li class='nav-item'><a class='nav-link' href='#'>Employees</a></li>
+</ul> 
+<h1>Sections:</h1>
 ";
 
 if ($result->num_rows > 0) {
@@ -22,14 +33,14 @@ if ($result->num_rows > 0) {
             <tr>
                 <td class='border-class'>".$row["SectionName"]."</td>
                 <td class='border-class'>".$row["SectionSize"]."</td>
-                <td><a href='sectionGiftshops.php/?sectionName=".$row["SectionName"]."'>Giftshops</a><td>
+                <td><a href='giftshops.php/?sectionName=".$row["SectionName"]."'>Giftshops</a><td>
                 <td><a href='#'>Concessions</a></td>
                 <td><a href='#'>Enclosures</a></td>
             </tr>
         ";
     }
     echo "</table>";
-    echo "<a href='createSection.html'>Create Section</a>";
+    echo "<br><a href='createSection.html'>Create Section</a>";
 } else {
     echo "0 results";
 }
